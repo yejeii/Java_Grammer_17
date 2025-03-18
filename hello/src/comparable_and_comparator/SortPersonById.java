f6ccfa94233ca186ab2f6c530d33c3d1ca24f758
yejeii <yesie0108@gmail.com>
2025-03-18 21:22:07 +0900

    rename: 컬렉션, 정렬 파일 관련 폴더명 수정

---
+++ hello/src/comparable_and_comparator/SortPersonById.java
@@ -0,0 +1,12 @@
+package comparable_and_comparator;
+
+import java.util.Comparator;
+
+public class SortPersonById implements Comparator<Person> {
+
+	@Override
+	public int compare(Person o1, Person o2) {
+		return o1.getId().compareTo(o2.getId());
+	}
+
+}
